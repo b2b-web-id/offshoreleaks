9b457ca933810b83584d43552d0ca6d1419c1e7b
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2016-05-30 10:15:26 +0700

    MySQL: add user if needed

--- data/load.sh
+++ data/load.sh
@@ -4,3 +4,8 @@ for i in *.sql.xz
 do
  xzcat $i | mysql -u root -p offshoreleaks -vvv
 done
+
+# Hilangkan komentar bila ingin menambahkan pengguna
+#mysql -u root -e "CREATE USER offshoreleaks@localhost IDENTIFIED BY ''" -p
+#mysql -u root -e "GRANT ALL PRIVILEGES ON offshoreleaks.* TO offshoreleaks@localhost" -p
+
